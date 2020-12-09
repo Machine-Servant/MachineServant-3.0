c53f87a9bd7386db9c0976682bab580169ca3dd7
Evan Stern <evanstern@machineservant.com>
2020-12-09 17:51:30 -0500

    Fix google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -98,33 +98,13 @@ module.exports = {
     `gatsby-plugin-fontawesome-css`,
     `gatsby-plugin-image`,
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: `gatsby-plugin-web-font-loader`,
       options: {
-        fonts: [
-          {
-            family: 'Raleway',
-            variants: [
-              '100',
-              '200',
-              '300',
-              '400',
-              '500',
-              '600',
-              '700',
-              '800',
-              '900',
-              '100i',
-              '200i',
-              '300i',
-              '400i',
-              '500i',
-              '600i',
-              '700i',
-              '800i',
-              '900i',
-            ],
-          },
-        ],
+        google: {
+          families: [
+            'Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900',
+          ],
+        },
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
