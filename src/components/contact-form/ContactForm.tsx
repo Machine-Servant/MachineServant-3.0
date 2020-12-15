60541b6b6e68de6325e2850e8bdcbcfd3244ac91
Evan Stern <evanstern@machineservant.com>
2020-12-15 13:38:18 -0500

    Fix accessibility issues (#36)

--- src/components/contact-form/ContactForm.tsx
+++ src/components/contact-form/ContactForm.tsx
@@ -24,7 +24,11 @@ export const ContactForm: React.FC = () => {
     >
       <input type="hidden" name="bot-field" />
       <input type="hidden" name="form-name" value="contact" />
+      <label htmlFor="contact-name" className="sr-only">
+        Name
+      </label>
       <Input
+        id="contact-name"
         type="text"
         name="name"
         placeholder="Name"
@@ -32,7 +36,11 @@ export const ContactForm: React.FC = () => {
         ref={register({ required: 'Please enter your name.' })}
       />
       {errors.name && <HelpText>{errors.name.message}</HelpText>}
+      <label htmlFor="contact-subject" className="sr-only">
+        Subject
+      </label>
       <Input
+        id="contact-subject"
         type="text"
         name="subject"
         placeholder="Subject"
@@ -40,7 +48,11 @@ export const ContactForm: React.FC = () => {
         ref={register({ required: 'Please enter a subject' })}
       />
       {errors.subject && <HelpText>{errors.subject.message}</HelpText>}
+      <label htmlFor="contact-email" className="sr-only">
+        Email
+      </label>
       <Input
+        id="contact-email"
         type="email"
         name="email"
         placeholder="Email"
@@ -48,7 +60,11 @@ export const ContactForm: React.FC = () => {
         ref={register({ required: 'Please enter an email' })}
       />
       {errors.email && <HelpText>{errors.email.message}</HelpText>}
+      <label htmlFor="contact-message" className="sr-only">
+        Message
+      </label>
       <Textarea
+        id="contact-message"
         className="mt-4"
         rows={7}
         name="message"
@@ -58,7 +74,7 @@ export const ContactForm: React.FC = () => {
       />
       {errors.message && <HelpText>{errors.message.message}</HelpText>}
       <button
-        className="w-full py-4 mt-4 text-sm text-white transition-colors duration-300 bg-lochmara-500 hover:bg-black"
+        className="w-full py-4 mt-4 text-sm font-bold text-white transition-colors duration-300 bg-lochmara-700 hover:bg-black"
         type="submit"
       >
         Submit
