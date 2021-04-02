e2718e304decb5e19ea9821b66e497fc0ce9c121
Evan Stern <evanstern@machineservant.com>
2021-04-02 08:39:57 -0400

    Update to react-hook-form v7

--- src/components/contact-form/ContactForm.tsx
+++ src/components/contact-form/ContactForm.tsx
@@ -5,7 +5,11 @@ import { useForm } from 'react-hook-form';
 import { HelpText, Input, Textarea } from './styles';
 
 export const ContactForm: React.FC = () => {
-  const { register, handleSubmit, errors } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   const onSubmit = useCallback(() => {
     const form = document.getElementsByName('contact')[0] as HTMLFormElement;
@@ -30,10 +34,9 @@ export const ContactForm: React.FC = () => {
       <Input
         id="contact-name"
         type="text"
-        name="name"
         placeholder="Name"
         error={!!errors.name}
-        ref={register({ required: 'Please enter your name.' })}
+        {...register('name', { required: 'Please enter your name.' })}
       />
       {errors.name && <HelpText>{errors.name.message}</HelpText>}
       <label htmlFor="contact-subject" className="sr-only">
@@ -42,10 +45,9 @@ export const ContactForm: React.FC = () => {
       <Input
         id="contact-subject"
         type="text"
-        name="subject"
         placeholder="Subject"
         error={!!errors.subject}
-        ref={register({ required: 'Please enter a subject' })}
+        {...register('subject', { required: 'Please enter a subject' })}
       />
       {errors.subject && <HelpText>{errors.subject.message}</HelpText>}
       <label htmlFor="contact-email" className="sr-only">
@@ -54,10 +56,9 @@ export const ContactForm: React.FC = () => {
       <Input
         id="contact-email"
         type="email"
-        name="email"
         placeholder="Email"
         error={errors.email}
-        ref={register({ required: 'Please enter an email' })}
+        {...register('email', { required: 'Please enter an email' })}
       />
       {errors.email && <HelpText>{errors.email.message}</HelpText>}
       <label htmlFor="contact-message" className="sr-only">
@@ -67,10 +68,9 @@ export const ContactForm: React.FC = () => {
         id="contact-message"
         className="mt-4"
         rows={7}
-        name="message"
         placeholder="Type your message here..."
         error={errors.message}
-        ref={register({ required: 'Please enter a message' })}
+        {...register('message', { required: 'Please enter a message' })}
       />
       {errors.message && <HelpText>{errors.message.message}</HelpText>}
       <button
